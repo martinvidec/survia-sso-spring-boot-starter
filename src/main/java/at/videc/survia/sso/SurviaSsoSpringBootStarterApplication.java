666c2f3a83c6ab752c5e6032562f71688e6d06b9
martinvidec <rx451g@gmail.com>
2024-04-16 20:53:57 +0200

    Initial commit

---
+++ src/main/java/at/videc/survia/sso/SurviaSsoSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package at.videc.survia.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurviaSsoSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurviaSsoSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/videc/survia/sso/SurviaSsoSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package at.videc.survia.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurviaSsoSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
